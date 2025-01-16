9b47b31ba1ba6d8b6661ef1b6faa898814f844a2
GonzaloAxelCode <gonzaloaxelcode@gmail.com>
2025-01-16 15:14:14 -0500

    Agregando mas cambios

--- workspaces/angular-app/src/app/app-routing.module.ts
+++ workspaces/angular-app/src/app/app-routing.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MultiplesComponent } from './components/multiples/multiples.component';
+
 
 const routes: Routes = [
-	{
-		path: '',
-		component: MultiplesComponent,
-	},
+
 ];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
 	exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- workspaces/angular-app/src/app/app.module.ts
+++ workspaces/angular-app/src/app/app.module.ts
@@ -6,7 +6,12 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MultiplesComponent } from './components/multiples/multiples.component';
+import { FormRegisterItemComponent } from './form-register-item/form-register-item.component';
+import { GridInventarioRegisterComponent } from './grid-inventario-register/grid-inventario-register.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { InventarioComponent } from './inventario/inventario.component';
+import { LoginComponent } from './login/login.component';
+
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
@@ -14,12 +19,17 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 }
 
 @NgModule({
-	declarations: [AppComponent, MultiplesComponent],
+	declarations: [AppComponent],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
 		ReactiveFormsModule,
+		InicioComponent,
+		LoginComponent,
+		InventarioComponent,
+		GridInventarioRegisterComponent,
+		FormRegisterItemComponent,
 		TranslateModule.forRoot({
 			defaultLanguage: 'en',
 			loader: {
@@ -32,4 +42,4 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
 	providers: [],
 	bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
